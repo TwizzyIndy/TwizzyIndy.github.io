3a8674f4ca56407d74b75492590529a16a3b587f
TwizzyIndy <twizzy.indy@gmail.com>
2016-11-01 02:31:15 +0630

    updated

--- js/mobile.js
+++ js/mobile.js
@@ -34,9 +34,9 @@ define([], function(){
         $tag = document.getElementById("js-tagcloud");
         $aboutme = document.getElementById("js-aboutme");
         $friends = document.getElementById("js-friends");
-        var tagStr = $tag?'<span class="viewer-title">标签</span><div class="viewer-div tagcloud" id="js-mobile-tagcloud"></div>':"";
-        var friendsStr = $friends?'<span class="viewer-title">友情链接</span><div class="viewer-div friends" id="js-mobile-friends"></div>':"";
-        var aboutmeStr = $aboutme?'<span class="viewer-title">关于我</span><div class="viewer-div aboutme" id="js-mobile-aboutme"></div>':"";
+        var tagStr = $tag?'<span class="viewer-title">TAG</span><div class="viewer-div tagcloud" id="js-mobile-tagcloud"></div>':"";
+        var friendsStr = $friends?'<span class="viewer-title">Friends</span><div class="viewer-div friends" id="js-mobile-friends"></div>':"";
+        var aboutmeStr = $aboutme?'<span class="viewer-title">AboutMe</span><div class="viewer-div aboutme" id="js-mobile-aboutme"></div>':"";
 
         $viewer.innerHTML = '<div id="viewer-box">\
         <div class="viewer-box-l">\
